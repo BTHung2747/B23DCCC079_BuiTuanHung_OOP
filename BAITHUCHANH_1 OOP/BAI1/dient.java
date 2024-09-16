786663532f4bc78d183d5ff69423d28b65f227b8
BTHung2747 <165037475+BTHung2747@users.noreply.github.com>
2024-09-16 13:23:20 +0700

    Update dient.java

--- BAITHUCHANH_1 OOP/BAI1/dient.java
+++ BAITHUCHANH_1 OOP/BAI1/dient.java
@@ -1,24 +1,32 @@
 /*
  * Bài 1
  */
-import java.util.Scanner;
-
 public class dient {
     public static void main(String[] args) {
-       Scanner sc= new Scanner(System.in);
-       long width = sc.nextLong(); 
-       long height = sc.nextLong();
-       long SXY=  width*height;
-       long CV=(width+height)*2;
-       if(SXY>0){
-        System.out.println(".(Diện tích hình cn là)"+ SXY);
-        System.err.println("Chu vi hình chữ nhật" + CV);
-       }
-       else{
-        System.out.println(".(Lỗi)");
-       }
+	    if (args.length < 2) {
+	      System.out.println("Vui lòng nhập hai giá trị a và b từ tham số dòng lệnh.");
+	      return;
+	    }
 
+	    if (args.length > 2) {
+	      System.out.println("Vui lòng chỉ nhập hai giá trị a và b.");
+	      return;
+	    }
 
-    }
+	    try {
+	      int a = Integer.parseInt(args[0]);
+	      int b = Integer.parseInt(args[1]);
 
+	      if (a > 0 && b > 0) {
+	        int cv = (a + b) * 2;
+	        int dt = a*b;
+	        System.out.println("Chu vi là: " + cv);
+	        System.out.println("Diện tích là:"+dt);
+	      } else {
+	        System.out.println("Giá trị a và b phải là số dương.");
+	      }
+	    } catch (NumberFormatException e) {
+	      System.out.println("Vui lòng nhập số hợp lệ cho a và b.");
+	    }
+	  }
 }
